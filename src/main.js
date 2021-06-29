f3f744b944735165495b806dead0e56ba083c0ba
Diva-fry <1263307831@qq.com>
2021-06-29 09:51:33 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from '@/App'
+
+//@是一个别名，是个小名，代表的就是我们的src路径
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
